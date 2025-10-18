3c78cbef70509fc7cf2cd47e16dc509a4d7b60d8
Verhalle,Koen K.G.T. <460975@student.fontys.nl>
2025-10-18 20:18:15 +0200

    Simpele tabel gemaakt om ritten weer te geven

--- BYR_WebApp/Pages/Ride/Actions.cshtml.cs
+++ BYR_WebApp/Pages/Ride/Actions.cshtml.cs
@@ -8,14 +8,14 @@ namespace BYR_WebApp.Pages.Ride
 {
     public class ActionsModel : PageModel
     {
+        //Core.Domain.Ride omdat Ride ook deze namespace is
+        public IReadOnlyList<Core.Domain.Ride> Rides { get; private set; } = [];
+
         public void OnGet()
         {
             //Alle ritten van de ingelogde customer ophalen
             //Voor nu 1 customer uit de database halen, straks via Cookies de ingelogde customer ophalen
-
-            //Core.Domain.Ride omdat Ride ook deze namespace is
-            List<Core.Domain.Ride> rides = CustomerService.GetRides(1);
-            
+            Rides = CustomerService.GetRides(1);
         }
 
         public void OnPost()
